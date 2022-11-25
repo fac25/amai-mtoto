588ffffe8b1a97feecc6553d87ae1e0b5ae1eadd
Sumithra-Suresh <jasisumi@gmail.com>
2022-11-25 12:14:16 +0000

    Added initial Files #34

--- pages/[trimester]/[topic].js
+++ pages/[trimester]/[topic].js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Login = () => {
+const Topic = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Topic
