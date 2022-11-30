91f2958877fab408fdd503e459ab67b35ec576d1
SSuresh <90449646+Sumithra-Suresh@users.noreply.github.com>
2022-11-30 11:41:25 +0000

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -26,27 +26,6 @@ import { useAuth } from "../context/AuthContext";
 import { useRouter } from "next/router";
 import ChakraButton from "./ChakraButton";
 
-// const Nav = () => {
-//   const { user, logOut, setUser } = useAuth();
-//   const router = useRouter();
-//   const handleLogout = async () => {
-//     try {
-//       await logOut();
-//       setUser(null);
-//       router.push("/landing-page");
-//     } catch (error) {
-//       // console.log(error.message);
-//     }
-//   };
-//   return (
-//     <div>
-//       <Logo />
-//       {user ? (
-//         <ChakraButton functionToCallWhenButtonIsClicked={handleLogout}>
-//           Logout
-//         </ChakraButton>
-//       ) : null}
-//     </div>
 const trimesterItems = createTrimesterItems();
 const navItems = [
   { label: "Home", href: "/home-page" },
@@ -105,7 +84,7 @@ export default function Nav() {
       setUser(null);
       router.push("/landing-page");
     } catch (error) {
-      // console.log(error.message);
+      console.log(error.message);
     }
   };
 
