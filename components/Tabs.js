c5fab02907d0ab8dde6fd99188d128a800f9158f
patdel0 <zefiru@protonmail.com>
2022-12-01 12:17:33 +0000

    Add trimester tabs integrated with url

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,12 +1,33 @@
+import next from "next";
+import { useRouter } from "next/router";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
-export default ({ tabs }) => {
-  const tabsLabel = tabs.map(({ name }) => <Tab>{name}</Tab>);
-  const tabsContent = tabs.map(({ content }) => <TabPanel>{content}</TabPanel>);
+export default ({ tabs, selectedTrimester }) => {
+  const router = useRouter();
+  const tabsLabel = tabs.map(({ name }, index) => (
+    <Tab
+      key={`tab-${index}`}
+      onClick={() => router.push(`/home-page?trimester=${index + 1}`)}
+    >
+      {name}
+    </Tab>
+  ));
+  const tabsContent = tabs.map(({ content }, index) => (
+    <TabPanel key={`tab-content-${index}`}>{content}</TabPanel>
+  ));
   return (
-    <Tabs>
+    <Tabs
+      defaultIndex={selectedTrimester - 1}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <TabList>{[...tabsLabel]}</TabList>
+      {[...tabsContent]}
     </Tabs>
   );
 };
--- pages/home-page.js
+++ pages/home-page.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Tabs from "../components/Tabs";
+import { Text } from "@chakra-ui/react";
 
-const HomePage = () => {
-  const tabs = [{ name: "Trimester 1", content: "I am content" }];
+export async function getServerSideProps({ query }) {
+  const selectedTrimester = query.trimester;
+  return {
+    props: {
+      selectedTrimester: selectedTrimester,
+    },
+  };
+}
+
+const HomePage = ({ selectedTrimester }) => {
+  const tabs = [
+    { name: "Trimester 1", content: <Text>I am content 1</Text> },
+    { name: "Trimester 2", content: <Text>I am content 2</Text> },
+    { name: "Trimester 3", content: <Text>I am content 3</Text> },
+  ];
   return (
     <Layout>
-      <Tabs tabs={tabs} />
+      <Tabs tabs={tabs} selectedTrimester={selectedTrimester} />
     </Layout>
   );
 };
