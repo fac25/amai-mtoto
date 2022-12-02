ccfee575f6d553981c7791f596a9f4fb5062896c
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-12-02 11:35:39 +0000

    Added new images to carousel

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/Layout";
 import ChakraButton from "../components/ChakraButton";
 import { useRouter } from "next/router";
 import { Flex } from "@chakra-ui/react";
+import WomanByWall from "../public/landingPageImages/Woman-by-wall.jpg";
+import WomanLookingUp from "../public/landingPageImages/woman-looking-up.jpg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -22,10 +24,7 @@ export async function getServerSideProps() {
 
 export default function Home() {
   const router = useRouter();
-  const mediaSrcArr = [
-    "https://plus.unsplash.com/premium_photo-1669052826135-aa5ca0417430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1669166717463-38a78c93412b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
-  ];
+  const mediaSrcArr = [WomanByWall.src, WomanLookingUp.src];
 
   const buttonTexts = [
     "Explore Trimester 1",
@@ -37,7 +36,7 @@ export default function Home() {
     buttonTexts.map((buttonText, i) => {
       e.target.innerText === buttonText &&
         // Needs to be changed to /summary when we have route
-        router.push(`/trimester-${i + 1}/exercise`);
+        router.push(`/trimester-${i + 1}/summary`);
     });
   }
 
@@ -45,9 +44,7 @@ export default function Home() {
     <Layout>
       <div className={styles.container}>
         <main>
-          <h1 className={styles.title}>
-            Hello to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+          <h1 className={styles.title}>Amai Mtoto</h1>
           <Carousel mediaSrcArr={mediaSrcArr} cardsHeight={"400px"} />
           <Flex>
             {buttonTexts.map((button, i) => {
@@ -85,7 +82,19 @@ function Carousel({ mediaSrcArr, cardsHeight }) {
               backgroundSize: "cover",
               height: cardsHeight,
             }}
-          ></SwiperSlide>
+          >
+            <div>
+              <p>
+                Amai Mtoto simply translates as mother and child, taken from the
+                languages from Shona (Zimbabwe) and Swahili (South Africa). This
+                pregnancy wellness app has been designed for those from a
+                Afro-Caribbean background to give you evidence based information
+                in the areas of exercise, nutrition and diet, mental wellbeing,
+                and parent education to help support and empower you and your
+                family through your pregnancy journey and beyond.
+              </p>
+            </div>
+          </SwiperSlide>
         );
       })}
     </Swiper>
