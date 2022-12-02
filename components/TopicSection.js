15680ea05e681bec3d87a73f61ea3720269d1131
patdel0 <zefiru@protonmail.com>
2022-12-02 11:00:43 +0000

    Add link to TopicSection to read more

--- components/TopicSection.js
+++ components/TopicSection.js
@@ -1,11 +1,12 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Link, Text } from "@chakra-ui/react";
 import React from "react";
 
-function TopicSection({ topicName, summary, children }) {
+function TopicSection({ topicName, summary, children, href }) {
   return (
     <div>
       <Heading>{topicName}</Heading>
       <Text>{summary}</Text>
+      <Link href={href}>Read more</Link>
       <div>{children}</div>
     </div>
   );
