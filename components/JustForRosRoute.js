87ea4cb87eac47b49fc43e833dca079ac385b568
y <johndoe@example.com>
2022-12-04 23:44:48 +0000

    Changed jfrr component a bit

--- components/JustForRosRoute.js
+++ components/JustForRosRoute.js
@@ -1,17 +1,22 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 
 const JustForRosRoute = ({ children }) => {
   const router = useRouter();
   const { user } = useAuth();
+  const [userIsRos, setUserIsRos] = useState(false);
+
+  let userIdOfRos = "GiripGLMxwhHF15iT8mwBzp4gzi1"; //for now this is jlo's uid just so i can test
 
   useEffect(() => {
-    if (!user || user.uid !== "ros's uid goes here") {
-      router.push("/log-in");
+    if (!user || user.uid !== userIdOfRos) {
+      router.push("/");
+    } else {
+      setUserIsRos(true);
     }
-  }, [router, user]);
-  return <div>{user ? children : null}</div>;
+  }, [router, user, userIdOfRos]);
+  return <div>{userIsRos ? children : null}</div>;
 };
 
 export default JustForRosRoute;
