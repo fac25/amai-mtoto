3c830bdd2d639c80d757b9827bfb4047ab258879
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-12-06 11:46:01 +0000

    Added brand name to logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import { Heading, Center } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Nav />
-      <Center>
-        <Heading>Amai Mtoto</Heading>
-      </Center>
       {children}
       <Footer />
     </div>
