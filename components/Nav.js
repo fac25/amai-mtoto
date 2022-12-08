6795ddf2058a864c908414630254d4e607ec6a54
Sumithra-Suresh <jasisumi@gmail.com>
2022-12-08 12:18:14 +0000

    Fix items disappearing on mobile view

--- components/Nav.js
+++ components/Nav.js
@@ -144,6 +144,7 @@ export default function Nav() {
               spacing={6}
             >
               <Button
+                className="sign-in"
                 as={"a"}
                 fontSize={"sm"}
                 fontWeight={400}
