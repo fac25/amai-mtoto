ef2619a978e66661a04e46e39821a91e5b864697
Sumithra-Suresh <jasisumi@gmail.com>
2022-12-09 11:56:25 +0000

    changed background color

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -21,12 +21,13 @@ const Disclaimer = () => {
         width={500}
         margin="0 auto"
         my={10}
+        className={"disclaimer_style"}
       >
         <AlertIcon boxSize="40px" mr={10} />
-        <AlertTitle mt={4} mb={1} fontSize="lg">
+        <AlertTitle mt={4} mb={1} fontSize="lg" className={"disclaimer_style"}>
           Disclaimer:
         </AlertTitle>
-        <AlertDescription maxWidth="sm">
+        <AlertDescription maxWidth="sm" className={"disclaimer_style"}>
           This app should be used as a guide and not as individualised care,
           although information is evidenced based, always refer to your
           healthcare professional (midwife or doctor), with any questions before
